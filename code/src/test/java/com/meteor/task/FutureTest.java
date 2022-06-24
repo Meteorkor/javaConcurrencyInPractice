6c5485326b1d909b30a9352af989bf0d40c72cf2
unseok.kim <unseok.kim@linecorp.com>
2022-06-24 23:41:22 +0900

    edit FutureTest, completableFuture

--- code/src/test/java/com/meteor/task/FutureTest.java
+++ code/src/test/java/com/meteor/task/FutureTest.java
@@ -1,9 +1,12 @@
 package com.meteor.task;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.Future;
 
 import org.junit.jupiter.api.Assertions;
@@ -11,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.util.concurrent.SettableListenableFuture;
 
 public class FutureTest {
+
     private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     @Test
@@ -52,9 +56,50 @@ public class FutureTest {
         Assertions.assertEquals(returnValue, settableListenableFuture.get());
     }
 
-//    TODO
-//    @Test
-//    void completableFutureTest() throws ExecutionException, InterruptedException {
-//
-//    }
+    @Test
+    void completableFutureTest() throws ExecutionException, InterruptedException {
+        String VALUE = "VALUE";
+        final CompletableFuture<String> stringCompletableFuture = CompletableFuture.completedFuture(VALUE);
+        Assertions.assertEquals(VALUE, stringCompletableFuture.get());
+    }
+
+    @Test
+    void completableFutureRunAsyncTest() throws ExecutionException, InterruptedException {
+        String VALUE = "VALUE";
+        final CompletableFuture<Void> voidCompletableFuture = CompletableFuture.runAsync(() -> {
+            //Thread.currentThread() : Thread[ForkJoinPool.commonPool-worker-19,5,main]
+            System.out.println("Thread.currentThread() : " + Thread.currentThread());
+        });
+
+        voidCompletableFuture.get();
+    }
+
+    @Test
+    void completableFutureRunAsyncCustomPoolNotWorkingTest() throws ExecutionException, InterruptedException {
+        String VALUE = "VALUE";
+        final ForkJoinPool forkJoinPool = new ForkJoinPool(10);
+        final ForkJoinTask<?> submit = forkJoinPool.submit(() -> {
+            final CompletableFuture<Void> voidCompletableFuture = CompletableFuture.runAsync(() -> {
+                //Thread.currentThread() : Thread[ForkJoinPool.commonPool-worker-19,5,main]
+                //custom notWorking
+                System.out.println("Thread.currentThread() : " + Thread.currentThread());
+            });
+        });
+
+        submit.get();
+    }
+
+    @Test
+    void completableFutureRunAsyncCustomPoolTest() throws ExecutionException, InterruptedException {
+        String VALUE = "VALUE";
+        final ForkJoinPool forkJoinPool = new ForkJoinPool(1);
+
+        final CompletableFuture<Void> voidCompletableFuture = CompletableFuture.runAsync(() -> {
+            //Thread[ForkJoinPool-1-worker-3,5,main]
+            System.out.println("Thread.currentThread() : " + Thread.currentThread());
+        }, forkJoinPool);
+
+        voidCompletableFuture.get();
+    }
+
 }
